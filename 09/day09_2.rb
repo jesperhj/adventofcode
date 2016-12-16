7a28a7104692b2a0ba2224c06d974ef831bede8b
Jesper Hjertstedt <jesper.hjertstedt@gmail.com>
2016-12-16 21:45:15 +0100

    day 9 problem 2

--- 09/day09_2.rb
+++ 09/day09_2.rb
@@ -1,7 +1,9 @@
 #!/bin/ruby
 
+# 22190123 low
+
 decompression_length = 0
-marker_states = [:length_to_repeat, :times_to_repeat, :getting_chars_to_repeat, :looking_for_marker]
+marker_states = [:length_to_repeat, :times_to_repeat, :fast_forward, :looking_for_marker]
 
 
 def found_start_marker(char)
@@ -17,59 +19,52 @@ def switch_to_repeat_to(char)
 end
 
 def decompress(line)
-  decompressed_line = ""
+  decompressed_length = 0
   position = 0
   state = :looking_for_marker
   length_to_repeat = ""
   times_to_repeat = ""
   chars_to_repeat = ""
+  ignore = 0
 
-  line.strip.each_char do |char|
-
-    if state == :looking_for_marker
+  (0..line.length-1). each do |i|
+    char = line[i]
+    if state == :fast_forward
+      ignore -= 1
+      state = :looking_for_marker if ignore == 0
+    elsif state == :looking_for_marker
       if found_start_marker(char)
         state = :length_to_repeat
       else
-        decompressed_line += char
+        decompressed_length += 1
       end
     else
-      if state == :times_to_repeat && found_end_marker(char)
-        state = :getting_chars_to_repeat
-      elsif state == :length_to_repeat && switch_to_repeat_to(char)
+      if found_end_marker(char)
+        new_line = line[i+1..i+length_to_repeat.to_i]
+        res = times_to_repeat.to_i * decompress(new_line)
+        decompressed_length += res
+        state = :fast_forward
+        ignore = length_to_repeat.to_i
+        length_to_repeat = ""
+        times_to_repeat = ""
+        chars_to_repeat = ""
+      elsif switch_to_repeat_to(char)
         state = :times_to_repeat
-      elsif state == :length_to_repeat
-        length_to_repeat += char
       elsif state == :times_to_repeat
         times_to_repeat += char
-      elsif state == :getting_chars_to_repeat
-        chars_to_repeat += char
-        # Are we ready to decompress?
-        if chars_to_repeat.length == length_to_repeat.to_i
-          puts "#{length_to_repeat}x#{times_to_repeat}"
-          (1..times_to_repeat.to_i).each { decompressed_line += chars_to_repeat }
-          state = :looking_for_marker
-          length_to_repeat = ""
-          times_to_repeat = ""
-          chars_to_repeat = ""
-        end
+      elsif state == :length_to_repeat
+        length_to_repeat += char
       end
     end
-    position += 0
   end
-  return decompressed_line
+  return decompressed_length
 end
 
 
 file = File.new("input.txt", "r")
 while (line = file.gets)
-
-  while line.count("(") > 0
-    puts line.count("(")
-    line = decompress(line)
-  end
-  puts line.length
-  decompression_length += line.length
+  decompression_length = decompress(line)
+  puts "#{decompression_length-1}"
 end
 file.close
 
-puts decompression_length
