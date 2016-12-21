aba0840fe7cff00865a52ec6087e669e85a31d12
Jesper Hjertstedt <jesper.hjertstedt@gmail.com>
2016-12-21 13:17:06 +0100

    day 21 problem 1

--- 21/day21_1.rb
+++ 21/day21_1.rb
@@ -1,10 +1,10 @@
-password = "abcde"
+password = "abcdefgh"
 
 class ScramblePassword
 
   def initialize(password)
     @password = password
-    @input_file = "input_test.txt"
+    @input_file = "input.txt"
   end
 
   def swap_position(x,y)
@@ -15,31 +15,43 @@ class ScramblePassword
 
   def swap_letter(x,y)
     (0..@password.length-1).each do |i|
-      if @password[]
+      if @password[i] == x
+        @password[i] = y
+      elsif @password[i] == y
+        @password[i] = x
+      end
     end
-    puts "swap letter #{x} with #{y}"
   end
 
   def rotate_direction(direction, steps)
-    puts "rotate #{direction} steps #{steps}"
+    @password = @password.split(//).rotate(steps * (direction == "right" ? -1 : 1)).join
   end
 
   def rotate_position(x)
-    puts "rotate letter #{x}"
+    position = (@password =~ /#{x}/).to_i
+    positions = -1*(1 + position + (position >= 4 ? 1 : 0))
+    @password = @password.split(//).rotate(positions).join
   end
 
   def reverse_position(x,y)
-    puts "reverse #{x} through #{y}"
+    pre = (x > 0) ? @password[0..x-1] : ""
+    to_reverse = @password[x..y]
+    post = (y < @password.length-1) ? @password[y+1, @password.length] : ""
+    @password = pre + to_reverse.reverse + post
   end
 
   def move_position(x,y)
-    puts "move #{x}, #{y}"
+    to_move = @password[x]
+    @password.slice!(x)
+    pre = (y > 0) ? @password[0..y-1] : ""
+    post = (y < @password.length) ? @password[y, @password.length - y] : ""
+    @password = pre + to_move + post
   end
 
   def parse_file
     file = File.new(@input_file, "r")
     while (line = file.gets)
-      puts @password
+      #puts line
       if res = /^swap position (\d+) with position (\d+)/.match(line)
         swap_position(res[1].to_i, res[2].to_i)
       elsif res = /^swap letter (\w+) with letter (\w+)/.match(line)
@@ -53,14 +65,13 @@ class ScramblePassword
       elsif res = /^move position (\d+) to position (\d+)/.match(line)
         move_position(res[1].to_i, res[2].to_i)
       end
-      puts @password
-      puts ""
     end
     file.close
   end
 
   def solve
     parse_file
+    puts @password
   end
 
 end
