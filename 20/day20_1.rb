bed35b1cb6c0b4ff3479a035382b907ed4d298ae
Jesper Hjertstedt <jesper.hjertstedt@gmail.com>
2016-12-21 21:55:29 +0100

    day 20 problem 1

--- 20/day20_1.rb
+++ 20/day20_1.rb
@@ -1,50 +1,22 @@
 #!/bin/ruby
+#4294967295
+# 22939479 to low
 
-valid = (0..4294967295).to_a
-puts "foo"
+lines = []
 file = File.new("input.txt", "r")
-while (line = file.gets)
-  puts "line #{line}"
-  input = /(\d+)-(\d+)/.match(line)
-  (input[1]..input[2]).each do |x|
-    valid.delete(x.to_i)
+  while (line = file.gets)
+    l = line.chomp.split(/-/)
+    lines.push([l[0].to_i, l[1].to_i])
+  end
+file.close
+
+lowest_number = 0
+lines.sort.each do |line|
+  puts "#{lowest_number} - #{line}"
+  if lowest_number >= line[0] && lowest_number <= line[1]
+    lowest_number = line[1]+1
+  elsif lowest_number < line[0]
+    puts "Loweset number #{lowest_number}"
+    break
   end
 end
-file.close
-puts "valid #{valid.first}"
-
-# 5-8
-#0-4, 9
-# 0-2
-# 4-7
-
-# valid = [[0,4294967295]]
-# valid = [[0,9]]
-# invalid = []
-# puts valid
-# file = File.new("input_test.txt", "r")
-# while (line = file.gets)
-#   puts "line #{line}"
-#   input = /(\d+)-(\d+)/.match(line)
-#   low = input[1].to_i
-#   high = input[2].to_i
-
-#   valid.each do |pair|
-#     if low < pair[0]
-#       if
-#     end
-#     if pair[0] < low && pair[1] > high
-#       valid.delete(pair)
-#       valid.push(low, high)
-#       break
-#     elsif pair[0] < low && pair[1] < high
-#       valid.delete(pair)
-#       valid.push([pair[0], low])
-#       valid.push([pair[1], high])
-#     end
-#   end
-#   puts "#{valid}"
-
-# end
-# file.close
-# puts "valid #{valid.first}"
