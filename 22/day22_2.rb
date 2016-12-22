2bd9fc0b56d25fa85118d808ca074a458cf35a01
Jesper Hjertstedt <jesper.hjertstedt@gmail.com>
2016-12-22 22:00:06 +0100

    day 22 problem 2, start moving around

--- 22/day22_2.rb
+++ 22/day22_2.rb
@@ -16,19 +16,32 @@ class GridComputing
         next unless line[0] == "/"
         input = /^\/dev\/grid\/node-x(\d+)-y(\d+)\s*(\d+)T\s*(\d+)T\s*(\d+)T\s*(\d+)%/.match(line)
         @nodes[[input[1].to_i,input[2].to_i]] = {
-          :size   => input[3].to_i,
-          :used   => input[4].to_i,
-          :avail  => input[5].to_i,
-          :use    => input[6].to_i
+          :size    => input[3].to_i,
+          :used    => input[4].to_i,
+          :avail   => input[5].to_i,
+          :use     => input[6].to_i,
+          :the_one => false
+
         }
-      #TODO set min and max
+      set_max(input[1].to_i, input[2].to_i)
       end
     file.close
   end
 
+  def set_max(x,y)
+    @min_x = x if x < @min_x
+    @max_x = x if x > @max_x
+    @min_y = y if y < @min_y
+    @max_y = y if y > @max_y
+  end
+
   def to_print(k,v)
+    if v[:the_one] == true
+    end
     if v[:used] == 0
       " _ "
+    elsif v[:the_one] == true
+      " G "
     elsif k[0] == 0 && k[1] == 0
       "(.)"
     else
@@ -50,32 +63,66 @@ class GridComputing
     end
   end
 
-  def move_data()
+  def move_node(node1, node2)
+    temp = @nodes[node1]
+    @nodes[node1] = @nodes[node2]
+    @nodes[node2] = temp
   end
 
   def done(node)
     node[0] == 0 && node[1] == 0 #&& TODO check storage
   end
 
-  def solve_inner(node, steps)
-    if node[-]
-    if node[0] > @min_x
-      #min x
+  def solve_inner(node1, node2, steps, stack)
+
+    puts "#{node1}-#{node2}"
+    stack[node1] = true
+    move_node(node1, node2) unless node1 == node2
+
+    if done(node2)
+      print
+      exit
     end
-    if node[0] < @max_x
-      #max x
+
+    if node2[0] > @min_x
+      if stack[[node2[0]-1, node2[1]]] == nil
+        if @nodes[[node2[0]-1, node2[1]]][:avail] >= @nodes[node2][:used]
+          solve_inner(node2,[node2[0]-1, node2[1]], steps+1, stack)
+        end
+      end
     end
-    if node[1] > @min_y
-      #min y
+    if node2[0] < @max_x
+      if stack[[node2[0]+1, node2[1]]] == nil
+        if @nodes[[node2[0]+1, node2[1]]][:avail] >= @nodes[node2][:used]
+          solve_inner(node2,[node2[0]+1, node2[1]], steps+1, stack)
+        end
+      end
     end
-    if node][1] < @max_y
+    if node2[1] > @min_x
+      if stack[[node2[0], node2[1]-1]] == nil
+        if @nodes[[node2[0], node2[1]-1]][:avail] >= @nodes[node2][:used]
+          solve_inner(node2,[node2[0], node2[1]-1], steps+1, stack)
+        end
+      end
+    end
+    if node2[1] < @max_x
+      if stack[[node2[0], node2[1]+1]] == nil
+        if @nodes[[node2[0], node2[1]+1]][:avail] >= @nodes[node2][:used]
+          solve_inner(node2,[node2[0], node2[1]+1], steps+1, stack)
+        end
+      end
     end
   end
 
   def solve
     start_node = find_start_node
+    set_the_one(@min_x, @max_y)
     steps = 0
-    solve_inner(start_node, steps)
+    solve_inner(start_node, start_node, steps, {})
+  end
+
+  def set_the_one(x,y)
+    @nodes[[x,y]][:the_one] = true
   end
 
   def find_start_node
