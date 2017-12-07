7d87d40d0ed7dc9256208cd251d5a7097230c3c0
Jesper Hjertstedt <jesper.hjertstedt@gmail.com>
2017-12-07 21:43:55 +0100

    day 4

---
+++ 2017/04/day04_1.rb
@@ -0,0 +1,13 @@
+#!/bin/ruby
+
+passwords = 0
+file = File.new("input.txt", "r")
+while (line = file.gets)
+  words = []
+  line.strip.split(" ").each do |w|
+    words.push(w)
+  end
+  passwords += words.size == words.uniq.size ? 1 : 0
+end
+file.close
+puts passwords
