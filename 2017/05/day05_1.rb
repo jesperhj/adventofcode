30748651034b0ac20472a0644914ea8b6b01ecbc
Jesper Hjertstedt <jesper.hjertstedt@gmail.com>
2017-12-07 22:07:12 +0100

    day 5

---
+++ 2017/05/day05_1.rb
@@ -0,0 +1,21 @@
+#!/bin/ruby
+
+message = []
+file = File.new("input.txt", "r")
+while (line = file.gets)
+  message.push(line.strip.to_i)
+end
+file.close
+
+steps = 0
+current_pos = 0
+
+while message[current_pos] != nil
+  steps += 1
+  old_pos = current_pos
+  if message[current_pos] != 0
+    current_pos = current_pos + message[current_pos]
+  end
+  message[old_pos] += 1
+end
+puts steps
