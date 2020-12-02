aed08c9c6fb135bb1139edf37907016a86538fbf
Jesper Hjertstedt <jesper.hjertstedt@gmail.com>
2020-12-02 14:28:31 +0100

    2020 day 1, part 2 in go

---
+++ 2020/01/day01_2.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+  "bufio"
+  "fmt"
+  "log"
+  "os"
+  "strconv"
+)
+
+func main() {
+  f, err := os.Open("input.txt")
+
+  if err != nil {
+    log.Fatal(err)
+  }
+  defer f.Close()
+
+  var lines []int
+  
+  scanner := bufio.NewScanner(f)
+  for scanner.Scan() {    
+    line, err := strconv.Atoi(scanner.Text())   
+    if err != nil {
+      log.Fatal(err)
+    }
+
+    lines = append(lines, line)
+  }
+
+
+  if err := scanner.Err(); err != nil {
+    log.Fatal(err)
+  }
+
+  for _, a := range lines {
+    for _, b := range lines {
+      for _, c := range lines {
+        if (a + b + c == 2020) {
+          fmt.Println(a*b*c)
+          return
+        }
+      }
+    }
+  }
+
+
+}
