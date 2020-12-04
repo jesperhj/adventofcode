8b1645aca12f0702f65c8505758950c49dc49133
Jesper Hjertstedt <jesper.hjertstedt@gmail.com>
2020-12-04 09:21:45 +0100

    2020 day 4 part 1 in ruby

---
+++ 2020/04/day04_1.rb
@@ -0,0 +1,23 @@
+new_passport = true
+lines = []
+l = ""
+file = File.new("input.txt", "r")
+while (line = file.gets)
+  if line.length == 1 || l == ""
+    lines << l if l != ""
+    l = line.strip
+  else
+    l = l + " " + line.strip
+  end
+end
+file.close
+lines << l if l != ""
+
+valid_passports = 0
+lines.each do |l|
+  if l.split.length == 8 || (l.split.length == 7 && !l.include?("cid"))
+    valid_passports += 1
+  end
+end
+
+puts valid_passports
