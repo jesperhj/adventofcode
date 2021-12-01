6f5a8c9969f25d861cd12c148d5a8271b6fc025d
Jesper Hjertstedt <jesper.hjertstedt@gmail.com>
2021-12-01 11:41:32 +0100

    2021 day 1 in ruby

---
+++ 2021/01/day01_2.rb
@@ -0,0 +1,21 @@
+report = []
+file = File.new("input.txt", "r")
+while (line = file.gets)
+  report.push(line.to_i)
+end
+file.close
+
+increases      = 0
+previous_depth = nil
+
+for i in 0..report.length()-1  
+  next if [0,report.length()-1].include?(i)
+  
+  depth = report[i-1]+report[i]+report[i+1]
+  if !previous_depth.nil? && depth > previous_depth
+    increases += 1
+  end
+  previous_depth = depth
+end
+
+puts increases
