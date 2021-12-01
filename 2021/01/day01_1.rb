dadb967c22586aca064c5e763d1e04257f8aae36
Jesper Hjertstedt <jesper.hjertstedt@gmail.com>
2021-12-01 19:03:21 +0100

    2021 day 1

---
+++ 2021/01/day01_1.rb
@@ -0,0 +1,18 @@
+report = []
+file = File.new("input.txt", "r")
+while (line = file.gets)
+  report.push(line.to_i)
+end
+file.close
+
+increases      = 0
+previous_depth = nil
+
+report.each do |depth|
+  if !previous_depth.nil? && depth > previous_depth
+    increases += 1
+  end
+  previous_depth = depth
+end
+
+puts increases
