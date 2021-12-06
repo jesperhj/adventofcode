b1c28ac45d69ff7b6d4f72e3d78695443d16025b
Jesper Hjertstedt <jesper.hjertstedt@gmail.com>
2021-12-06 22:44:35 +0100

    2021 day 6 part 1

--- 2021/06/day06_1.rb
+++ 2021/06/day06_1.rb
@@ -1,17 +1,35 @@
 class Lanternfish
   def initialize()
     @lanterns = []    
-
+    @days     = 256
     self.get_input    
   end
   
   def get_input    
-    file = File.new("input_test.txt", "r")
+    file = File.new("input.txt", "r")
     @lanterns = file.gets.split(",").map {|x| x.to_i}
     file.close    
   end
   
-  def solve    
+  def solve
+    puts @lanterns.join(",")
+    
+    (0..@days-1).each do |d|
+      new_lanters = []
+      for i in 0..@lanterns.length-1
+        if @lanterns[i] == 0
+          @lanterns[i] = 6
+          new_lanters.push(8)
+        else
+          @lanterns[i] -= 1
+        end
+      end
+
+      @lanterns = @lanterns.concat(new_lanters)
+    end
+    
+    
+    puts @lanterns.count
   end
 end
 
