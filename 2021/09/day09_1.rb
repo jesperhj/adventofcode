ff96a4cc6ce374eecef610f6f1ce5699f337315d
Jesper Hjertstedt <jesper.hjertstedt@gmail.com>
2021-12-09 19:19:21 +0100

    2021 day 9 part 1

--- 2021/09/day09_1.rb
+++ 2021/09/day09_1.rb
@@ -1,3 +1,4 @@
+# 1901 too high
 class SmokeBasin
   def initialize()
     @tubes      = []
@@ -6,7 +7,7 @@ class SmokeBasin
   end
 
   def get_input
-    file = File.new("input_test.txt", "r")
+    file = File.new("input.txt", "r")
     while (line = file.gets)
       @tubes.push line.strip.split("").map! {|x| x.to_i}
     end
@@ -17,14 +18,14 @@ class SmokeBasin
     for i in 0..@tubes.length-1
       for j in 0..@tubes[i].length-1
         is_low = true
-        is_low = false if i > 0 && @tubes[i][j] > @tubes[i-1][j]
-        is_low = false if i < @tubes.length-2 && @tubes[i][j] < @tubes[i+1][j]
-        is_low = false if j > 0 && @tubes[i][j] < @tubes[i][j-1]
-        is_low = false if j < @tubes[i].length-2 && @tubes[i][j] < @tubes[i][j+1]
+        is_low = false if i > 0 && @tubes[i][j] >= @tubes[i-1][j]
+        is_low = false if i < @tubes.length-1 && @tubes[i][j] >= @tubes[i+1][j]
+        is_low = false if j > 0 && @tubes[i][j] >= @tubes[i][j-1]
+        is_low = false if j < @tubes[i].length-1 && @tubes[i][j] >= @tubes[i][j+1]        
         @low_points.push @tubes[i][j] if is_low
       end
-    end
-    puts @low_points
+    end    
+    puts @low_points.sum {|x|x+1}
   end
 end
 
