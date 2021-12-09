f405aacb092810b44fb668f61e0dc7f287d360fa
Jesper Hjertstedt <jesper.hjertstedt@gmail.com>
2021-12-09 19:01:49 +0100

    2021 day 9 part 1

---
+++ 2021/09/day09_1.rb
@@ -0,0 +1,32 @@
+class SmokeBasin
+  def initialize()
+    @tubes      = []
+    @low_points = []
+    self.get_input
+  end
+
+  def get_input
+    file = File.new("input_test.txt", "r")
+    while (line = file.gets)
+      @tubes.push line.strip.split("").map! {|x| x.to_i}
+    end
+    file.close
+  end
+
+  def solve
+    for i in 0..@tubes.length-1
+      for j in 0..@tubes[i].length-1
+        is_low = true
+        is_low = false if i > 0 && @tubes[i][j] > @tubes[i-1][j]
+        is_low = false if i < @tubes.length-2 && @tubes[i][j] < @tubes[i+1][j]
+        is_low = false if j > 0 && @tubes[i][j] < @tubes[i][j-1]
+        is_low = false if j < @tubes[i].length-2 && @tubes[i][j] < @tubes[i][j+1]
+        @low_points.push @tubes[i][j] if is_low
+      end
+    end
+    puts @low_points
+  end
+end
+
+program = SmokeBasin.new()
+program.solve
