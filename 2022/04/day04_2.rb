627c46d5f71fc0bdd5af8edd567ca32ad60d3539
Jesper Hjertstedt <jesper.hjertstedt@gmail.com>
2022-12-05 22:34:07 +0100

    2022 day4

---
+++ 2022/04/day04_2.rb
@@ -0,0 +1,24 @@
+asssignments = []
+file = File.new("input.txt", "r")
+while (line = file.gets)
+    pair = []
+    pair << line.split(",")[0].split("-")
+    pair << line.split(",")[1].split("-")
+    asssignments.push(pair)
+end
+file.close
+
+cointained_pair = 0
+asssignments.each do |a|
+    if (a[0][0].to_i >= a[1][0].to_i && a[0][0].to_i <= a[1][1].to_i)
+        cointained_pair += 1
+    elsif  (a[0][1].to_i >= a[1][0].to_i && a[0][1].to_i <= a[1][1].to_i)
+        cointained_pair += 1
+    elsif (a[1][0].to_i >= a[0][0].to_i && a[1][0].to_i <= a[0][1].to_i)
+        cointained_pair += 1
+    elsif (a[1][1].to_i >= a[0][0].to_i && a[1][1].to_i <= a[0][1].to_i)
+        cointained_pair += 1
+    end
+end
+
+puts cointained_pair
